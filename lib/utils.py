63bed993fbb7f9556c6d85056ef576391243f516
lifapan <lifapan@matrixelements.com>
2022-08-18 15:35:22 +0800

    updade get_block_count_number func

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,8 @@ import time
 
 import rlp
 from loguru import logger
+from platon_aide.economic import Economic, new_economic
+from platon_aide.utils import ec_recover
 
 
 def get_pledge_list(func, nodeid=None) -> list:
@@ -49,3 +51,26 @@ def withdraw_delegate_reward_gas_fee(aide, staking_num, uncalcwheels, gas_price=
     gas = get_dynamic_gas_parameter(data) + 8000 + 3000 + 21000 + staking_num * 1000 + uncalcwheels * 100
     return gas * gas_price
 
+
+def get_block_count_number(aide, node_id=None, current_block=None, roundnum=1):
+    """
+        Get the number of blocks out of the verification node
+        """
+    if current_block is None:
+        current_block = aide.platon.block_number
+    if node_id is None:
+        node_id = aide.staking._node_id
+
+    block_namber = aide.economic.consensus_blocks * roundnum
+    count = 0
+    for i in range(block_namber):
+        if current_block > 0:
+            block = aide.platon.get_block(current_block)
+            public_key = ec_recover(block)
+            # node_id = get_pub_key(node.url, current_block)
+            current_block = current_block - 1
+            if node_id == public_key:
+                count = count + 1
+        else:
+            break
+    return count
