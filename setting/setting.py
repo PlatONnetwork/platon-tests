9957ee736dbf4e430753679829f6a8b76859c1b9
Shinnng <shinnng@outlook.com>
2022-11-22 11:05:15 +0800

    some optimizetion

--- setting/setting.py
+++ setting/setting.py
@@ -3,9 +3,16 @@ import sys
 from datetime import datetime
 from loguru import logger
 
+# 日志设置
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+log_file = datetime.strftime(datetime.now(), 'log/log-%m%d%H%M%S.log')
+logger.add(log_file, level="INFO")
+
+# 项目路径
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SCHEME = 'http'     # ws/wss/http/https
+SCHEME = 'http'  # ws/wss/http/https
 NETWORK = 'private'
 
 # 部署链的配置文件
@@ -17,7 +24,7 @@ PLATON = os.path.join(BASE_DIR, 'env-files/bin/platon')
 VERSION = '1.2.0'
 
 # 历史版本，通常使用线上版本，用于验证从该历史版本升级到测试版本的过程
-HISTORY_PLATON = os.path.join(BASE_DIR, 'env-files/bin/history/platon')
+HISTORY_PLATON = os.path.join(BASE_DIR, '../env-files/history/platon')
 HISTORY_VERSION = '1.1.0'
 
 # 治理测试版本
@@ -25,9 +32,3 @@ PIP_BIN_DIR = os.path.join(BASE_DIR, 'env-files/pip-bin')
 
 # keystore
 KEYSTORE = os.path.join(BASE_DIR, 'env-files/keystore')
-
-# 日志设置
-# logger.remove()
-# logger.add(sys.stderr, level="INFO")
-log_file = datetime.strftime(datetime.now(), 'log/log-%m%d%H%M%S.log')
-logger.add(log_file, level="DEBUG")
